c3ba85a03edf0257ee7683cffa0dcf92d5315297
Ilce <ilsemilena1706@gmail.com>
2023-11-15 12:33:45 -0500

    refactorizacion parte del codigo

---
+++ src/components/buttonsFilters.js
@@ -0,0 +1,6 @@
+<div class="buttons">
+  <button id="applyFilter">Aplicar</button>
+  <button id="clearFilter" data-testid="button-clear">
+    Borrar
+  </button>
+</div>;
---
+++ src/components/footer.js
@@ -0,0 +1,5 @@
+<footer>
+  <i class="fa-brands fa-github" style="color: #d7d700"></i>
+  <a href="https://github.com/vaneval27">By Vane</a>
+  <a href="https://github.com/1Tai7/DEV012-dataverse">& Ilce</a>
+</footer>;
