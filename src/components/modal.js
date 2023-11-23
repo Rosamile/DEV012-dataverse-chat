23eb11f182926faf7a36b581be295c633d6e0907
Rosamile <138530369+Rosamile@users.noreply.github.com>
2023-11-23 11:12:30 -0500

    se mueve carpeta de router

---
+++ src/components/modal.js
@@ -0,0 +1,18 @@
+
+const modal = () => {
+    const view = `
+    <section class="containermodal">
+      <article class="containeroverlay">
+        <article class="popup">
+          <h3>Elije una opción</h3>
+          <label for="chatgroup">Selecciona un personaje</label>
+          <select class="chatindividual" id="chatindividual" >meter la data</select>
+          <button class="button" id="">Chat Grupal</button>
+          <button class="button" id="btncerrar"><i class="fa-solid fa-xmark" id="imgcerrar"></i></button>
+        </article>
+      </article>
+    </section>
+    `;
+    return view;
+    };
+    export default modal;
