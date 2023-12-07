b23f37bf2f9d88d94977a4bf711cf1f12dfb7054
Ilce <ilsemilena1706@gmail.com>
2023-12-07 15:08:17 -0500

    correcion apikey

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,29 +1,31 @@
-/*const API_KEY = "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";
-const prompt = sectionchatG.querySelector("#writeHere");
 
-async function getCompletion(prompt) {
-  const rest = awaitfetch("https://api.openai.com/v1/chat/completions", {
+
+function getCompletion(prompt, name) {
+  const apiKEY = localStorage.getItem("password") || API_KEY;
+
+  const resIA = fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Bearer" + API_KEY,
+      Authorization: "Bearer " + apiKEY,
     },
-    body: JSON.stringify({ model: "gpt-3.5-turbo" }),
-    messages: [
-      {
-        role: "user",
-        content: prompt,
-      },
-    ],
-  });
-  return await rest.json();
+    body: JSON.stringify({
+      temperature: 0,
+      max_tokens: 60,
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Tu nombre es ${name}`,
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    }),
+  }).then((res) => res.json());
+  return resIA;
 }
 
-const button = sectionchatG.querySelector("#buttonEnviarChatG");
-const output = sectionchatG.querySelector("#textUser");
-
-button.addEventListener("click", async () => {
-  if (!prompt.value) return;
-  const response = await getCompletion(prompt.value);
-  output.innerHTML = response.choice[0].messages.content;
-});*/
+export default getCompletion;
